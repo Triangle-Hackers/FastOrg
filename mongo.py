41d500c97f2318b4cefb1366a674b9b687c73277
cadenedam <cedam@purdue.edu>
2025-02-21 22:48:04 -0500

    csv to mongo

--- mongo.py
+++ mongo.py
@@ -6,4 +6,6 @@ uri = "mongodb+srv://cadenedam:z2G3nyHF0Hfpn7UE@cluster0.e1ur4.mongodb.net/?retr
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-#
+print("Databases:")
+for db_name  in client.list_database_names():
+    print(f"- {db_name}")
