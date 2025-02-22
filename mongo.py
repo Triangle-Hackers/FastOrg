08dd3025c614c62b1cfe6af36f42dfda01ee8de8
cadenedam <cedam@purdue.edu>
2025-02-21 22:13:24 -0500

    venv

--- mongo.py
+++ mongo.py
@@ -6,9 +6,4 @@ uri = "mongodb+srv://cadenedam:z2G3nyHF0Hfpn7UE@cluster0.e1ur4.mongodb.net/?retr
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+#
