4e692ce12af2673e77853c0f708d7107ff2b5f9e
AndreiDea <adeacone@purdue.edu>
2025-02-22 15:25:47 -0500

    Update App.jsx

--- frontend-src/src/App.jsx
+++ frontend-src/src/App.jsx
@@ -4,7 +4,6 @@ import Home from './views/Home';
 import AIRequest from './views/AIRequest';
 import LandingPage from './views/LandingPage';
 import NotFound from './views/NotFound';
-import AIController from './controllers/AIControl';
 
 const App = () => {
   const [isSessionValid, setIsSessionValid] = useState(false);
