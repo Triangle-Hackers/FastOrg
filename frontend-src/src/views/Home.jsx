265ff0841fda706b62dc47c139c736fc0bb4e69a
cadenedam <cedam@purdue.edu>
2025-02-23 02:05:17 -0500

    Dashboard Dark Mode

--- frontend-src/src/views/Home.jsx
+++ frontend-src/src/views/Home.jsx
@@ -21,8 +21,12 @@ const Home = ({
     useEffect(() => {
         if (!getCompletedSetup()) {
             navigate('/setup');
-
+        }
     }, []);
+
+    const [mode, setMode] = useState(() => {
+            return localStorage.getItem('mode') || 'light';
+        });
 
     const [isEditMode, setIsEditMode] = useState(false);
     const [editedRoster, setEditedRoster] = useState(roster);
@@ -53,7 +57,7 @@ const Home = ({
 
 
     return (
-        <div className="home-container">
+        <div className={`home-container ${mode}`}>
             <Navbar />
 
             <div className="main-content-wrapper">
