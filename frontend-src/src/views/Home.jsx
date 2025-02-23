60aad9cbac84219871f667eed7f7425e40293a6e
cadenedam <cedam@purdue.edu>
2025-02-23 02:11:43 -0500

    Dark mode implemented

--- frontend-src/src/views/Home.jsx
+++ frontend-src/src/views/Home.jsx
@@ -24,7 +24,7 @@ const Home = ({
         }
     }, []);
 
-    const [mode, setMode] = useState(() => {
+    const [mode] = useState(() => {
             return localStorage.getItem('mode') || 'light';
         });
 
